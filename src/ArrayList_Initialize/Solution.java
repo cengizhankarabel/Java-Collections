db63e9fbb0e5e88dd7c33bf9d76fded9a63201ae
CENGIZHAN KARABEL <69279953+cengizhankarabel@users.noreply.github.com>
2022-01-23 21:56:57 -0600

    "ArrayList Initialize"

---
+++ src/ArrayList_Initialize/Solution.java
@@ -0,0 +1,24 @@
+package ArrayList_Initialize;
+
+/*
+    ---> ArrayList Initialize
+
+ */
+
+import java.util.ArrayList;
+
+public class Solution {
+    public static void main(String[] args) {
+
+        ArrayList<Integer> arrayList = new ArrayList<Integer>();
+
+        arrayList.add(3);
+        arrayList.add(4);
+        arrayList.add(5);
+        arrayList.add(6);
+        arrayList.add(7);
+        arrayList.add(8);
+
+        System.out.println(arrayList);
+    }
+}
